55a4ef1cde876daa2d790204342e5a9e74e5edeb
Adnan Chahbandar <me@adnan.io>
2021-01-24 23:08:55 -0600

    fixed mobile sidebar ;

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,7 +6,7 @@
     justify="center"
     class="container px-8 color"
   >
-    <v-col class="border ma-8 flex-grow-0 flex-shrink-1" cols="12">
+    <v-col class="border d-none d-lg-block ma-8 flex-grow-0 flex-shrink-1" cols="12">
       <Banner />
       <HelloWorld />
       <h2 class="text-center">
@@ -14,33 +14,48 @@
         <v-btn v-if="form" dark @click="showForm" class="my-8">back</v-btn>
       </h2>
     </v-col>
-    <!-- <v-col class="border ma-8 d-lg-none flex-grow-1 flex-shrink-1" cols="12">
+    <v-col v-if="!form" class="border ma-8 d-lg-none flex-grow-1 flex-shrink-1" cols="12">
       <Banner />
-      <HelloWorld />
+      <HelloWorldMobile />
       <h2 class="text-center">
         <v-btn v-if="!form" dark @click="showForm" class="my-6">Contact Us</v-btn>
         <v-btn v-if="form" dark @click="showForm" class="my-6">back</v-btn>
       </h2>
-    </v-col> -->
-    <transition
-      appear
-      name="slide-x-transition"
-    >
+    </v-col>
+
+      <v-col
+        transition="slide-x-transition"
+        cols="12"
+        v-if="form"
+        class="color fill mt-n12 mb-n16 d-lg-none"
+      >
+        <transition
+          appear
+          name="slide-x-transition"
+        >
+          <Sidebar/>
+        </transition>
+      </v-col>
       <v-col
         transition="slide-x-transition"
         cols="6"
         v-if="form"
-        class="color fill mt-n12 mb-n16"
+        class="color fill mt-n12 mb-n16 d-none d-lg-block"
+      >
+      <transition
+        appear
+        name="slide-x-transition"
       >
         <Sidebar/>
-      </v-col>
-    </transition>
+      </transition>
+    </v-col>
   </v-row>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorldMobile from '@/components/HelloWorldMobile.vue'
 import Banner from '@/components/Banner.vue'
 import Sidebar from '@/components/Sidebar.vue'
 
@@ -48,6 +63,7 @@ export default {
   name: 'Home',
   components: {
     HelloWorld,
+    HelloWorldMobile,
     Banner,
     Sidebar
   },
